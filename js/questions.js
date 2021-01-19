e40ab181537a761e0c9f2173d624045c0cfe43bb
SemHiel <30608@ma-web.nl>
2021-01-19 10:11:03 +0100

    vlugge update

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Wat voor museum is het LAM?",
+    answer: "Een food-art museum",
+    options: [
+      "Een geschiedenis museum",
+      "Een sex-museum",
+      "Een food-art museum",
+      "Een kunst museum"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Waar ligt het LAM?",
+    answer: "Lisse",
+    options: [
+      "Lisse",
+      "Amsterdam",
+      "Ede",
+      "Rotterdam"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Voor welke leeftijd is het LAM?",
+    answer: "Elke leeftijd",
+    options: [
+      "21+",
+      "Iedereen onder de 18",
+      "Elke leeftijd",
+      "18+"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Wat heeft het LAM?",
+    answer: "Bijzondere architectuur",
+    options: [
+      "Standaard Kunst",
+      "Veel Schilderijen.",
+      "Bijzondere architectuur",
+      "Van Gogh"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Zou je naar het LAM komen?",
+    answer: "Ja!",
+    options: [
+      "Ja!",
+      "Nee, Helaas.",
+      "Misschien",
+      "Weet ik nog niet."
+    ]
+  },
+];
